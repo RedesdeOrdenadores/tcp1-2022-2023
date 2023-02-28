88afea8dc3c4621e92093c98aa7e7f83550e62a8
Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
2023-02-28 13:05:48 +0100

    Use i8 numbers for values

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 use std::array::TryFromSliceError;
-use std::num::ParseIntError;
+use std::num::{NonZeroI8, ParseIntError, TryFromIntError};
 use std::{fmt::Display, str::FromStr};
 use thiserror::Error;
 use tlv::TlvType;
@@ -18,6 +18,8 @@ pub enum TCPLibError {
     Parse,
     #[error("Not enough data in TLV")]
     NotEnoughData(#[from] TryFromSliceError),
+    #[error("Invalid parameter")]
+    InvalidParameter(#[from] TryFromIntError),
     #[error("Could not parse integer")]
     ParseIntError(#[from] ParseIntError),
     #[error("Something wrong")]
@@ -57,22 +59,22 @@ impl From<i64> for Answer {
 
 #[derive(Clone, Copy, Debug)]
 pub enum Operation {
-    Sum { a: u8, b: u8 },
-    Sub { a: u8, b: u8 },
-    Mul { a: u8, b: u8 },
-    Div { a: u8, b: u8 },
-    Rem { a: u8, b: u8 },
+    Sum { a: i8, b: i8 },
+    Sub { a: i8, b: i8 },
+    Mul { a: i8, b: i8 },
+    Div { a: i8, b: NonZeroI8 },
+    Rem { a: i8, b: NonZeroI8 },
     Fact(u8),
 }
 
 impl Operation {
     pub fn reduce(self) -> i64 {
         match self {
-            Operation::Sum { a, b } => (a as u16 + b as u16).into(),
+            Operation::Sum { a, b } => (a as i16 + b as i16).into(),
             Operation::Sub { a, b } => (a as i16 - b as i16).into(),
-            Operation::Mul { a, b } => (a as u16 * b as u16).into(),
-            Operation::Div { a, b } => (a / b).into(),
-            Operation::Rem { a, b } => (a % b).into(),
+            Operation::Mul { a, b } => (a as i16 * b as i16).into(),
+            Operation::Div { a, b } => (a / b.get()).into(),
+            Operation::Rem { a, b } => (a % b.get()).into(),
             Operation::Fact(0) => 1,
             Operation::Fact(a) => (1..=a as i64)
                 .reduce(|acc, e| acc.saturating_mul(e))
@@ -81,11 +83,15 @@ impl Operation {
     }
     pub fn encode(self) -> Box<[u8]> {
         match self {
-            Operation::Sum { a, b } => Tlv::encode(TlvType::Sum, &[a, b]).unwrap(),
-            Operation::Sub { a, b } => Tlv::encode(TlvType::Sub, &[a, b]).unwrap(),
-            Operation::Mul { a, b } => Tlv::encode(TlvType::Mul, &[a, b]).unwrap(),
-            Operation::Div { a, b } => Tlv::encode(TlvType::Div, &[a, b]).unwrap(),
-            Operation::Rem { a, b } => Tlv::encode(TlvType::Rem, &[a, b]).unwrap(),
+            Operation::Sum { a, b } => Tlv::encode(TlvType::Sum, &[a as u8, b as u8]).unwrap(),
+            Operation::Sub { a, b } => Tlv::encode(TlvType::Sub, &[a as u8, b as u8]).unwrap(),
+            Operation::Mul { a, b } => Tlv::encode(TlvType::Mul, &[a as u8, b as u8]).unwrap(),
+            Operation::Div { a, b } => {
+                Tlv::encode(TlvType::Div, &[a as u8, b.get() as u8]).unwrap()
+            }
+            Operation::Rem { a, b } => {
+                Tlv::encode(TlvType::Rem, &[a as u8, b.get() as u8]).unwrap()
+            }
             Operation::Fact(a) => Tlv::encode(TlvType::Fact, &[a]).unwrap(),
         }
     }
@@ -95,26 +101,27 @@ impl<'a> TryFrom<Tlv<'a>> for Operation {
     type Error = TCPLibError;
 
     fn try_from(tlv: Tlv) -> Result<Self, Self::Error> {
+        let data: Box<[_]> = tlv.data.iter().map(|&u| u as i8).collect();
         Ok(match tlv.tag {
             TlvType::Sum if tlv.length == 2 => Operation::Sum {
-                a: tlv.data[0],
-                b: tlv.data[1],
+                a: data[0],
+                b: data[1],
             },
             TlvType::Sub if tlv.length == 2 => Operation::Sub {
-                a: tlv.data[0],
-                b: tlv.data[1],
+                a: data[0],
+                b: data[1],
             },
             TlvType::Mul if tlv.length == 2 => Operation::Mul {
-                a: tlv.data[0],
-                b: tlv.data[1],
+                a: data[0],
+                b: data[1],
             },
-            TlvType::Div if tlv.length == 2 && tlv.data[1] != 0 => Operation::Div {
-                a: tlv.data[0],
-                b: tlv.data[1],
+            TlvType::Div if tlv.length == 2 => Operation::Div {
+                a: data[0],
+                b: data[1].try_into()?,
             },
-            TlvType::Rem if tlv.length == 2 && tlv.data[1] != 0 => Operation::Rem {
-                a: tlv.data[0],
-                b: tlv.data[1],
+            TlvType::Rem if tlv.length == 2 => Operation::Rem {
+                a: data[0],
+                b: data[1].try_into()?,
             },
             TlvType::Fact if tlv.length == 1 => Operation::Fact(tlv.data[0]),
             _ => return Err(TCPLibError::Generic),
@@ -139,7 +146,7 @@ impl FromStr for Operation {
     type Err = TCPLibError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let regex = Regex::new(r"^\s*([0-9]+)\s*([+\-*×x/÷%!])\s*([0-9]+)?").unwrap();
+        let regex = Regex::new(r"^\s*(\-?[0-9]+)\s*([+\-*×x/÷%!])\s*(\-?[0-9]+)?\s*$").unwrap();
         let elements: Box<_> = match regex.captures(s) {
             Some(captures) => captures.iter().skip(1).collect(),
             None => return Err(TCPLibError::Parse),
@@ -149,7 +156,7 @@ impl FromStr for Operation {
             [Some(match_a), Some(_), Some(match_b)] => {
                 (match_a.as_str().parse()?, match_b.as_str().parse()?)
             }
-            [Some(match_a), Some(_), None] => (match_a.as_str().parse()?, 0u8),
+            [Some(match_a), Some(_), None] => (match_a.as_str().parse()?, 0i8),
             _ => {
                 return Err(TCPLibError::Parse);
             }
@@ -159,9 +166,15 @@ impl FromStr for Operation {
             Some("+") if elements[2].is_some() => Operation::Sum { a, b },
             Some("-") if elements[2].is_some() => Operation::Sub { a, b },
             Some("*" | "×" | "x") if elements[2].is_some() => Operation::Mul { a, b },
-            Some("/" | "÷") if elements[2].is_some() => Operation::Div { a, b },
-            Some("%") if elements[2].is_some() => Operation::Rem { a, b },
-            Some("!") if elements[2].is_none() => Operation::Fact(a),
+            Some("/" | "÷") if elements[2].is_some() => Operation::Div {
+                a,
+                b: b.try_into()?,
+            },
+            Some("%") if elements[2].is_some() => Operation::Rem {
+                a,
+                b: b.try_into()?,
+            },
+            Some("!") if elements[2].is_none() => Operation::Fact(a.try_into()?),
             Some(op) => return Err(TCPLibError::UnsupportedOperation(op.to_string())),
             None => return Err(TCPLibError::Parse),
         };
